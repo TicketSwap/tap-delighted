7611b6f273af2ba1fb0594a8cd6abf4c1005f461
Tobias Cadée <tobias.cadee@ticketswap.com>
2023-12-07 14:55:56 +0100

    fix: :bug: fix tests (#6)

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import datetime
+from datetime import datetime, timedelta
 import os
 
 from singer_sdk.testing import get_tap_test_class
@@ -8,8 +8,8 @@ from singer_sdk.testing import get_tap_test_class
 from tap_delighted.tap import TapDelighted
 
 SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    "api_key": os.getenv('TAP_DELIGHTED_API_KEY_NPS')
+    "start_date": (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d"),
+    "api_key": os.getenv("TAP_DELIGHTED_API_KEY_NPS"),
 }
 
 
